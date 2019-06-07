c1dda210ca8821c1e58ed368f19316b57c1388ce
Carl <carl.paton@gmail.com>
2019-06-07 12:53:00 +1200

    Added libman.json for client side libs, initial for info mvc

---
+++ WebApp/Controllers/InfoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisRepository.enums;
+using RedisRepository.Interfaces;
+
+namespace WebApp.Controllers
+{
+    public class InfoController : Controller
+    {
+        private readonly IRedisRepository _redisRepository;
+
+        public InfoController(IRedisRepository redisRepository)
+        {
+            _redisRepository = redisRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var redisServerInformation = _redisRepository.Info(InfoEnum.Info.all);
+            return View();
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,7 +29,6 @@ namespace WebApp
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
 
             //DI ~ Appsettings
             var appSettings = new ConfigurationBuilder()

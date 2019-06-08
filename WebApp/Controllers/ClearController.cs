fd158fb8df538a8b6b385122b2c06534b205ca40
Carl Paton <carl.paton@gmail.com>
2019-06-08 15:35:15 +1200

    Clear/flushall

---
+++ WebApp/Controllers/ClearController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisRepository.Interfaces;
+using WebApp.Services;
+
+namespace WebApp.Controllers
+{
+    public class ClearController : Controller
+    {
+        private readonly IRedisRepository _redisRepository;
+
+        public ClearController(IRedisRepository redisRepository)
+        {
+            _redisRepository = redisRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Clear()
+        {
+            _redisRepository.Clear();
+
+            new SetTempDataMessage()
+                .Display(TempData, "OK", "Data cleared.");
+
+            return View("Index");
+        }
+    }
+}
